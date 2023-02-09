06a6d250bcaeae906fc11697c795f4da0c6ddfec
eSTee3 <31021627+eSTee3@users.noreply.github.com>
2023-02-09 13:02:12 -0600

    Add files via upload

---
+++ utils/inputCheck.js
@@ -0,0 +1,9 @@
+const checkInputs = (name, password) => {
+    if (name == '') {
+        alert('Username');
+    } if (password == '') {
+        alert('Password');
+    }
+}
+
+module.exports = checkInputs;
