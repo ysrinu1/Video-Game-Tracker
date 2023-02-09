d06956ea69d6b614452399c9c76cc4dfa4deaa18
eSTee3 <31021627+eSTee3@users.noreply.github.com>
2023-02-09 12:59:12 -0600

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,10 +1,6 @@
 const Gamers = require('./gamers');
 const Games = require('./games');
 
-// A single Gamer can have many games
-Gamers.hasMany(Games, {
-    foreignKey: 'game_id'
-});
 // Each Game belongs to a single gamer
 Games.belongsTo(Gamers, {
     foreignKey: 'gamer_id'
