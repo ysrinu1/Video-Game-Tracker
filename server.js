b7cbfbb7c56e89f1fe6d37a0e3a848c45a81e895
eSTee3 <31021627+eSTee3@users.noreply.github.com>
2023-02-09 13:05:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const path = require('path');
-const express = require('express');
+const exphbs = require('express-handlebars'); 
+const routes = require('./controllers'); 
+const helpers = require('./utils/helpers'); 
+const express = require('express'); 
 const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
+const sequelize = require('./config/connection'); 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
  
 const sess = {
   secret: 'Super secret secret',
@@ -32,13 +32,14 @@ const sess = {
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-// app.engine('handlebars', hbs.engine);
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
+
+sequelize.sync();
 
 app.listen(PORT, () => console.log(`Now listening on port http://localhost:${PORT}`));
