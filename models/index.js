1a123b4117bc802ce7d9f579b4f3aa3a9d06c9ce
eSTee3 <tagsxa@yahoo.com>
2023-02-08 18:26:51 -0600

    Added model placeholders

--- models/index.js
+++ models/index.js
@@ -1,6 +1,18 @@
-/*************************************************
-* 
-* The purpose of this file is to create "one-to-many" 
-* and "one-to-one" relationships with your models
-*
-**************************************************/
+const Gamers = require('./gamers');
+const Games = require('./games');
+const Genres = require('./genres');
+
+// A single Gamer can have many games
+Gamers.hasMany(Games, {
+    foreignKey: 'game_id'
+});
+// Each Game belongs to a single gamer
+Games.belongsTo(Gamer, {
+    foreignKey: 'gamer_id'
+});
+// Each user can have many comments
+Games.hasMany(Genres, {
+    foreignKey: 'genre_id'
+});
+
+module.exports = { Gamers, Games, Genres };
