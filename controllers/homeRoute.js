bf02acdfb5faa4198dd8b952c5458469878914f7
eSTee3 <tagsxa@yahoo.com>
2023-02-12 11:11:52 -0600

    Added new landing page

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { YourCustomModel } = require('../models');
+const { User, Game } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
     // Pass serialized data and session flag into template
-    res.render('signup', { 
+    res.render('home', { 
     });
   } catch (err) {
     res.status(500).json(err);
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoute = require('./homeRoutes');
+const homeRoute = require('./homeRoute');
 const dashboardRoute = require('./dashbaordRoute');
 
 router.use('/', homeRoute);
