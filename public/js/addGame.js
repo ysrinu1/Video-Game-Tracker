ec6ce97975275289dabc261aead150ad1aa764f2
eSTee3 <31021627+eSTee3@users.noreply.github.com>
2023-02-13 10:29:45 -0600

    Update addGame.js

--- public/js/addGame.js
+++ public/js/addGame.js
@@ -1,21 +1,30 @@
 const addGame = async (e) => {
     e.preventDefault(); 
 
-    const contents = $('#modal-content-textarea').val();
-    const title = $('#modal-title-input').val().trim();
-   if  (contents && title) {
-    const response = await fetch(`/api/post`, {
+const title = $('#game-title').val().trim();
+const system = $('#game-system').val().trim();
+const genre = $('#game-genre').val().trim();
+const rating = $('#game-rating').val().trim();
+const summary = $('#game-summary').val();
+const tips_tricks = $('#game-tips_tricks').val();
+
+   if (title && system && genre && rating && summary && tips_tricks) {
+    const response = await fetch(`/api/game`, {
         method: 'POST', 
-        body: JSON.stringify({ title, contents}), 
+        body: JSON.stringify({ title, system, genre, rating, summary, tips_tricks }), 
         headers: { 'Content-Type': 'application/json' }
     });
-    const data = await response.json();
 
-    if (response.ok) {
-        alert(data.message)
-        document.location.replace('/dashboard');
-    } else {
-        alert(`I'm sorry but I've ran into an issue while attempting to create your post.`);
+    const gameData = await response.json();
+        
+    if(response.status === 400 || response.status === 404) { 
+       return alert(gameData.message)
+        }
+    if(response.ok){
+        //replaces current page with home page
+        document.location.replace('/');
+    }else {
+        alert(`I'm sorry but I've ran into an issue while attempting to create your game.`);
     }
    }
     
@@ -23,4 +32,4 @@ const addGame = async (e) => {
     
 }
 
-$('#add-post').click(addGame);
+$('#add-game').click(addGame);
