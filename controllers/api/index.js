ee7a82082b469df043ca88a128f6fdac65df5079
eSTee3 <31021627+eSTee3@users.noreply.github.com>
2023-02-13 10:27:22 -0600

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const loginRoute = require('./loginRoute');
 const logoutRoute = require('./logoutRoute');
 const signupRoute = require('./signupRoute');
-const gameEntryRoute = require('./gameEntryRoutes');
+const gameEntryRoute = require('./gameRoute');
 
 
 router.use('/login', loginRoute);
 router.use('/logout', logoutRoute);
 router.use('/signup', signupRoute);
-router.use('/gameentry', gameEntryRoute);
+router.use('/game', gameEntryRoute);
 
 module.exports = router;
